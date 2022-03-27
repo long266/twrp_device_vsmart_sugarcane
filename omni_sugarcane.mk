280ed96bbf55b872079dc667ae35c8285a397c0c
long266 <71440530+long266@users.noreply.github.com>
2022-03-27 13:40:51 +0700

    Update omni_sugarcane.mk

--- omni_sugarcane.mk
+++ omni_sugarcane.mk
@@ -3,7 +3,7 @@ $(call inherit-product, vendor/omni/config/common.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
 ifeq ($(TARGET_PREBUILT_KERNEL),)
-	LOCAL_KERNEL := device/vsmart/sugarcane/Image.gz-dtb
+	LOCAL_KERNEL := device/vsmart/sugarcane/prebuilt/zImage-dtb
 else
 	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
 endif
@@ -30,7 +30,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 	ro.product.device=sugarcane \
 	ro.build.date.utc=1000000000
 
-BUILD_FINGERPRINT := vsmart/casuarina_open/casuarina:10/QKQ1.200311.002/V430A_OPN_U_B15_211112:user/release-keys
+BUILD_FINGERPRINT := vsmart/sugarcane_open/sugarcane:10/QKQ1.200311.002/V330A_OPN_U_B9_201209:user/release-keys
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE=sugarcane \
     PRODUCT_NAME=sugarcane
